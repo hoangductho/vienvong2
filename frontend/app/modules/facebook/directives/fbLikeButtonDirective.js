b7aa970a14a0c688f9a3394cd7bff22200e4c384
Hoàng Đức Thọ <hoangductho.3690@gmail.com>
2015-06-24 00:04:48 +0700

    frontend: fix bug facebook like box not load 2

--- frontend/app/modules/facebook/directives/fbLikeButtonDirective.js
+++ frontend/app/modules/facebook/directives/fbLikeButtonDirective.js
@@ -3,12 +3,24 @@
  */
 angular
     .module('facebook')
-    .directive('fbLikeButton', function () {
+    .directive('fbLikeButton', function ($compile) {
         return {
             restrict: 'C',
             scope: {
-                link: '='
             },
-            templateUrl: 'modules/facebook/views/fbLikeButton.html'
+            link: function (scope, element, attrs) {
+                var observer = function(link) {
+                    // build markdown editor template.
+                    var newElement = $compile(
+                        '<div class="fb-like" data-href="' + link + '"'
+                       +'data-layout="button_count" data-action="like" data-show-faces="false" data-share="true">'
+                       +'</div>')(scope);
+
+                    // add markdown editor in to point called it. html() doesn't work
+                    element.replaceWith(newElement);
+
+                };
+                attrs.$observe('link', observer);
+            }
         }
     });
