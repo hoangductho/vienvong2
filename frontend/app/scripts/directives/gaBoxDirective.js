088f63d106c8a11d94ba2ca9101473fcaebca58b
Hoàng Đức Thọ <hoangductho.3690@gmail.com>
2015-06-30 14:33:31 +0700

    frontend: del async GA js 4

--- frontend/app/scripts/directives/gaBoxDirective.js
+++ frontend/app/scripts/directives/gaBoxDirective.js
@@ -20,15 +20,6 @@ angular
                         || (attrs.maxscreen && $window.innerWidth < attrs.maxscreen)
                         || (!attrs.maxscreen && !attrs.minscreen)
                     ) {
-                        if(typeof(adsbygoogle) == "undefined") {
-                            jQuery.ajax({
-                                type: "GET",
-                                url: "//pagead2.googlesyndication.com/pagead/js/adsbygoogle.js",
-                                success: function(){},
-                                dataType: "script",
-                                cache: true
-                            });
-                        };
 
                         // build markdown editor template.
                         var newElement = $compile(
